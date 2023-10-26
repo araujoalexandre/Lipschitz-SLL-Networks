fix numerical stability & Lipschitzness

core/evaluate.py
@@ -70,10 +70,7 @@ class Evaluator:
     self.load_ckpt()
     if self.config.mode == "certified":
       for eps in [36, 72, 108, 255]:
-        if self.config.last_layer == 'lln':
-          self.eval_certified_lln(eps)
-        else:
-          self.eval_certified(eps)
+        self.eval_certified(eps)
     elif self.config.mode == "attack":
       self.eval_attack()
 
@@ -88,37 +85,7 @@ class Evaluator:
     running_inputs = 0
     lip_cst = 1.
     data_loader, _ = self.reader.load_dataset()
-    for batch_n, data in enumerate(data_loader):
-      inputs, labels = data
-      inputs, labels = inputs.cuda(), labels.cuda()
-      outputs = self.model(inputs)
-      predicted = outputs.argmax(axis=1)
-      correct = outputs.max(1)[1] == labels
-      margins = torch.sort(outputs, 1)[0]
-      certified = (margins[:, -1] - margins[:, -2]) > np.sqrt(2.) * lip_cst * eps_float
-      running_accuracy += predicted.eq(labels.data).cpu().sum().numpy()
-      running_certified += torch.sum(correct & certified).item()
-      running_inputs += inputs.size(0)
-    self.model.train()
-    accuracy = running_accuracy / running_inputs
-    certified = running_certified / running_inputs
-    self.message.add('eps', [eps, 255], format='.0f')
-    self.message.add('eps', eps_float, format='.5f')
-    self.message.add('accuracy', accuracy, format='.5f')
-    self.message.add('certified accuracy', certified, format='.5f')
-    logging.info(self.message.get_message())
-    return accuracy, certified
-
-  @torch.no_grad()
-  def eval_certified_lln(self, eps):
-    eps_float = eps / 255
-    self.model.eval()
-    running_accuracy = 0
-    running_certified = 0
-    running_inputs = 0
-    lip_cst = 1.
-    data_loader, _ = self.reader.load_dataset()
-    last_weight = self.model.module.model.last_last.weight
+    last_weight = self.model.module.model.model[-1].weight
     normalized_weight = F.normalize(last_weight, p=2, dim=1)
     for batch_n, data in enumerate(data_loader):
       inputs, labels = data

core/models/layers.py
@@ -6,64 +6,83 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+def safe_inv(x):
+  mask = x == 0
+  x_inv = x**(-1)
+  x_inv[mask] = 0
+  return x_inv
+
 
 class SDPBasedLipschitzConvLayer(nn.Module):
 
-  def __init__(self, config, input_size, cin, cout, kernel_size=3, epsilon=1e-6):
+  def __init__(self, cin, inner_dim, kernel_size=3, stride=1):
     super(SDPBasedLipschitzConvLayer, self).__init__()
 
-    self.activation = nn.ReLU(inplace=False)
+    inner_dim = inner_dim if inner_dim != -1 else cin
+    self.activation = nn.ReLU()
 
-    self.kernel = nn.Parameter(torch.empty(cout, cin, kernel_size, kernel_size))
-    self.bias = nn.Parameter(torch.empty(cout))
-    self.q = nn.Parameter(torch.randn(cout))
+    self.padding = kernel_size // 2
+
+    self.kernel = nn.Parameter(torch.randn(inner_dim, cin, kernel_size, kernel_size))
+    self.bias = nn.Parameter(torch.empty(1, inner_dim, 1, 1))
+    self.q = nn.Parameter(torch.randn(inner_dim))
 
     nn.init.xavier_normal_(self.kernel)
     fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.kernel)
     bound = 1 / np.sqrt(fan_in)
     nn.init.uniform_(self.bias, -bound, bound) # bias init
 
-    self.epsilon = epsilon
+  def compute_t(self):
+    ktk = F.conv2d(self.kernel, self.kernel, padding=self.kernel.shape[-1] - 1)
+    ktk = torch.abs(ktk)
+    q = torch.exp(self.q).reshape(-1, 1, 1, 1)
+    q_inv = torch.exp(-self.q).reshape(-1, 1, 1, 1)
+    t = (q_inv * ktk * q).sum((1, 2, 3))
+    t = safe_inv(t)
+    return t
 
   def forward(self, x):
-    res = F.conv2d(x, self.kernel, bias=self.bias, padding=1)
-    res = self.activation(res)
-    batch_size, cout, x_size, x_size = res.shape
-    kkt = F.conv2d(self.kernel, self.kernel, padding=self.kernel.shape[-1] - 1)
-    q_abs = torch.abs(self.q)
-    T = 2 / (torch.abs(q_abs[None, :, None, None] * kkt).sum((1, 2, 3)) / q_abs)
-    res = T[None, :, None, None] * res
-    res = F.conv_transpose2d(res, self.kernel, padding=1)
+    t = self.compute_t()
+    t = t.reshape(1, -1, 1, 1)
+    res = F.conv2d(x, self.kernel, padding=1)
+    res = res + self.bias
+    res = t * self.activation(res)
+    res = 2 * F.conv_transpose2d(res, self.kernel, padding=1)
     out = x - res
-    return out  
+    return out
+
 
 
 class SDPBasedLipschitzLinearLayer(nn.Module):
 
-  def __init__(self, config, cin, cout, epsilon=1e-6):
+  def __init__(self, cin, inner_dim):
     super(SDPBasedLipschitzLinearLayer, self).__init__()
 
-    self.activation = nn.ReLU(inplace=False)
-    self.weights = nn.Parameter(torch.empty(cout, cin))
-    self.bias = nn.Parameter(torch.empty(cout))
-    self.q = nn.Parameter(torch.rand(cout))
+    inner_dim = inner_dim if inner_dim != -1 else cin
+    self.activation = nn.ReLU()
 
-    nn.init.xavier_normal_(self.weights)
-    fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weights)
+    self.weight = nn.Parameter(torch.empty(inner_dim, cin))
+    self.bias = nn.Parameter(torch.empty(1, inner_dim))
+    self.q = nn.Parameter(torch.randn(inner_dim))
+
+    nn.init.xavier_normal_(self.weight)
+    fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
     bound = 1 / np.sqrt(fan_in)
-    nn.init.uniform_(self.bias, -bound, bound)  # bias init
+    nn.init.uniform_(self.bias, -bound, bound) # bias init
 
-    self.epsilon = epsilon
+  def compute_t(self):
+    q = torch.exp(self.q)
+    q_inv = torch.exp(-self.q)
+    t = torch.abs(torch.einsum('i,ik,kj,j -> ij', q_inv, self.weight, self.weight.T, q)).sum(1)
+    t = safe_inv(t)
+    return t
 
   def forward(self, x):
-    res = F.linear(x, self.weights, self.bias)
-    res = self.activation(res)
-    q_abs = torch.abs(self.q)
-    q = q_abs[None, :]
-    q_inv = (1/(q_abs+self.epsilon))[:, None]
-    T = 2/torch.abs(q_inv * self.weights @ self.weights.T * q).sum(1)
-    res = T * res
-    res = F.linear(res, self.weights.t())
+    t = self.compute_t()
+    res = F.linear(x, self.weight)
+    res = res + self.bias
+    res = t * self.activation(res)
+    res = 2 * F.linear(res, self.weight.T)
     out = x - res
     return out
 

core/models/model.py
@@ -23,11 +23,11 @@ class LipschitzNetwork(nn.Module):
   def __init__(self, config, n_classes):
     super(LipschitzNetwork, self).__init__()
 
-    self.depth = config.depth
-    self.num_channels = config.num_channels
-    self.depth_linear = config.depth_linear
-    self.n_features = config.n_features
-    self.conv_size = config.conv_size
+    self.n_conv= config.n_conv
+    self.n_dense = config.n_dense
+    self.cin = config.w
+    self.conv_inner_dim = config.conv_inner_dim
+    self.dense_inner_dim = config.dense_inner_dim
     self.n_classes = n_classes
 
     if config.dataset == 'tiny-imagenet':
@@ -35,40 +35,29 @@ class LipschitzNetwork(nn.Module):
     else:
       imsize = 32
 
-    self.conv1 = PaddingChannels(self.num_channels, 3, "zero")
+    self.model = []
+    self.model.append(
+      PaddingChannels(self.cin, 3, "zero")
+    )
 
-    layers = []
-    block_conv = SDPBasedLipschitzConvLayer
-    block_lin = SDPBasedLipschitzLinearLayer
-
-    for _ in range(self.depth):
-      layers.append(block_conv(config, (1, self.num_channels, imsize, imsize), self.num_channels, self.conv_size))
+    for _ in range(self.n_conv):
+      self.model.append(SDPBasedLipschitzConvLayer(self.cin, self.conv_inner_dim))
     
+    self.model.append(nn.AvgPool2d(4, divisor_override=4))
 
-    layers.append(nn.AvgPool2d(4, divisor_override=4))
-    self.stable_block = nn.Sequential(*layers)
-
-    layers_linear = [nn.Flatten()]
+    self.model.append(nn.Flatten())
     if config.dataset in ['cifar10', 'cifar100']:
-      in_channels = self.num_channels * 8 * 8
+      in_channels = self.cin * 8 * 8
     elif config.dataset == 'tiny-imagenet':
-      in_channels = self.num_channels * 16 * 16
+      in_channels = self.cin * 16 * 16
 
-    for _ in range(self.depth_linear):
-      layers_linear.append(block_lin(config, in_channels, self.n_features))
+    for _ in range(self.n_dense):
+      self.model.append(SDPBasedLipschitzLinearLayer(in_channels, self.dense_inner_dim))
 
-    if config.last_layer == 'pooling_linear':
-      self.last_last = PoolingLinear(in_channels, self.n_classes, agg="trunc")
-    elif config.last_layer == 'lln':
-      self.last_last = LinearNormalized(in_channels, self.n_classes)
-    else:
-      raise ValueError("Last layer not recognized")
-
-
-    self.layers_linear = nn.Sequential(*layers_linear)
-    self.base = nn.Sequential(*[self.conv1, self.stable_block, self.layers_linear])
+    self.model.append(LinearNormalized(in_channels, self.n_classes))
+    self.model = nn.Sequential(*self.model)
 
   def forward(self, x):
-    return self.last_last(self.base(x))
+    return self.model(x)
 
 

core/utils.py
@@ -15,7 +15,7 @@ import torch.nn.functional as F
 import torch.distributed as dist
 from torch.optim import lr_scheduler
 from advertorch import attacks
-from autoattack import AutoAttack
+# from autoattack import AutoAttack
 import pytorch_warmup as warmup
 
 

reproduce_tables.py
@@ -15,37 +15,30 @@ class Config:
   def __init__(self, dataset, size):
     self.mode = 'certified'
     self.dataset = dataset
-    self.data_dir = '/gpfsscratch/rech/yxj/uuc79vj/data:/gpfswork/rech/yxj/uuc79vj/data:/gpfswork/rech/yxj/uuc79vj/data/vision_datasets:/gpfsdswork/dataset'
     self.ngpus = 4
     self.batch_size = 500
     self.shift_data = True
-    self.first_layer = 'padding_channels'
-    if dataset in ['cifar100', 'tiny-imagenet']:
-      self.last_layer = 'lln' 
-    else:
-      self.last_layer = 'pooling_linear'
 
     if size == 'small':
-      config = self.set_archi(20, 45, 7, 2048)
+      config = self.set_archi(20, 7, 45, 2048)
     elif size == 'medium':
-      config = self.set_archi(30, 60, 10, 2048)
+      config = self.set_archi(30, 10, 60, 2048)
     elif size == 'large':
-      config = self.set_archi(50, 90, 10, 2048)
+      config = self.set_archi(50, 10, 90, 2048)
     elif size == 'xlarge':
-      config = self.set_archi(70, 120, 15, 2048)
+      config = self.set_archi(70, 15, 120, 2048)
 
-  def set_archi(self, depth, num_channels, depth_linear, n_features):
-    self.depth = depth
-    self.num_channels = num_channels
-    self.depth_linear = depth_linear
-    self.n_features = n_features
-    self.conv_size = 5
+  def set_archi(self, n_conv, n_dense, w, dense_inner_dim):
+    self.n_conv = n_conv
+    self.n_dense = n_dense
+    self.conv_inner_dim = 5
+    self.dense_inner_dim = dense_inner_dim
+    self.w = w
 
 
 def load_ckpt(model, ckpt_path):
   checkpoint = torch.load(ckpt_path)['model_state_dict']
   model.load_state_dict(checkpoint)
-
 
 @torch.no_grad()
 def eval_certified(model, reader):
@@ -55,32 +48,7 @@ def eval_certified(model, reader):
   running_inputs = 0
   lip_cst = 1.
   data_loader, _ = reader.load_dataset()
-  for batch_n, data in enumerate(data_loader):
-    inputs, labels = data
-    inputs, labels = inputs.cuda(), labels.cuda()
-    outputs = model(inputs)
-    predicted = outputs.argmax(axis=1)
-    correct = outputs.max(1)[1] == labels
-    margins = torch.sort(outputs, 1)[0]
-    running_accuracy += predicted.eq(labels.data).cpu().sum().numpy()
-    for i, eps in enumerate([36, 72, 108, 255]):
-      eps_float = eps / 255
-      certified = (margins[:, -1] - margins[:, -2]) > np.sqrt(2.) * lip_cst * eps_float
-      running_certified[i] += torch.sum(correct & certified).item()
-    running_inputs += inputs.size(0)
-  accuracy = running_accuracy / running_inputs
-  certified = running_certified / running_inputs
-  return accuracy, certified
-
-@torch.no_grad()
-def eval_certified_lln(model, reader):
-  model.eval()
-  running_accuracy = 0
-  running_certified = torch.zeros(4)
-  running_inputs = 0
-  lip_cst = 1.
-  data_loader, _ = reader.load_dataset()
-  last_weight = model.module.model.last_last.weight
+  last_weight = model.module.model.model[-1].weight
   normalized_weight = F.normalize(last_weight, p=2, dim=1)
   for batch_n, data in enumerate(data_loader):
     inputs, labels = data
@@ -118,6 +86,7 @@ def main():
   message = utils.MessageBuilder()
 
   for dataset in datasets:
+
     print(f'-- Evaluation on {dataset} --')
     print('+-----------------------------------------------+')
     print('| {:>5}  |  {:>5}  |  {:>5}  |  {:>5}  |  {:>5} |'.format(
@@ -131,7 +100,7 @@ def main():
 
       # load reader
       Reader = readers_config[dataset]
-      reader = Reader(config, batch_size, False, is_training=False)
+      reader = Reader(config, 256, False, is_training=False)
       config.means = reader.means
 
       # load model
@@ -144,10 +113,7 @@ def main():
       avg_certified = torch.zeros(4)
       for ckpt_path in paths:
         load_ckpt(model, ckpt_path)
-        if not config.last_layer == 'lln':
-          accuracy, certified = eval_certified(model, reader)
-        else:
-          accuracy, certified = eval_certified_lln(model, reader)
+        accuracy, certified = eval_certified(model, reader)
         avg_accuracy += accuracy
         avg_certified += certified
 